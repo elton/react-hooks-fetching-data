5e9a264dad758df6be72508dc10fe7f2b269320b
Elton Zheng <elton.zheng.sh@gmail.com>
2020-08-29 13:33:49 +0800

    fetchind data with useState, useReducer hooks, and use custom hook to extract for data fetching.

--- src/App.js
+++ src/App.js
@@ -1,32 +1,42 @@
-import React, { useEffect, useState } from "react";
-import axios from "axios";
+import React, { useState } from "react";
+import useDataApi from "./api/useDataApi";
 
 function App() {
-  const [data, setData] = useState({ hits: [] });
   const [query, setQuery] = useState("redux");
-  useEffect(() => {
-    const fetchData = async () => {
-      const result = await axios(
-        `https://hn.algolia.com/api/v1/search?query=${query}`
-      );
-      setData(result.data);
-    };
-    fetchData();
-  }, [query]);
+  const [{ data, isLoading, isError }, doFetch] = useDataApi(
+    "https://hn.algolia.com/api/v1/search?query=redux",
+    {
+      hits: [],
+    }
+  );
+
   return (
     <>
-      <input
-        type="text"
-        value={query}
-        onChange={(e) => setQuery(e.target.value)}
-      />
-      <ul>
-        {data.hits.map((item) => (
-          <li key={item.objectID}>
-            <a href={item.uri}>{item.title}</a>
-          </li>
-        ))}
-      </ul>
+      <form
+        onSubmit={(e) => {
+          doFetch(`http://hn.algolia.com/api/v1/search?query=${query}`);
+          e.preventDefault();
+        }}
+      >
+        <input
+          type="text"
+          value={query}
+          onChange={(e) => setQuery(e.target.value)}
+        />
+        <button type="submit">Search</button>
+      </form>
+      {isError && <div>Something went wrong...</div>}
+      {isLoading ? (
+        <div>Loading</div>
+      ) : (
+        <ul>
+          {data.hits.map((item) => (
+            <li key={item.objectID}>
+              <a href={item.uri}>{item.title}</a>
+            </li>
+          ))}
+        </ul>
+      )}
     </>
   );
 }
